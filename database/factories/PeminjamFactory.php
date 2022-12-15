f2eeb11d04b2ad457426516eda75fb103530b8c7
PerwiraDhani <perwira.dzakwan11@gmail.com>
2022-12-15 20:53:13 +0700

    added branch peminjam

---
+++ database/factories/PeminjamFactory.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Peminjam;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class PeminjamFactory extends Factory
+{
+    protected $model = Peminjam::class;
+
+    public function definition(): array
+    {
+    	return [
+    	    //
+    	];
+    }
+}

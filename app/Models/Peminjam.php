cfc04a064e0f04a3e7efaada44017b924beab546
PerwiraDhani <perwira.dzakwan11@gmail.com>
2022-12-16 07:15:37 +0700

    update Peminjam models

--- app/Models/Peminjam.php
+++ app/Models/Peminjam.php
@@ -3,9 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Book;
+
 
 class Peminjam extends Model
 {
     //
+    public function book()
+    {
+        return $this->hasMany(Book::class);
+    }
+
     protected $fillable = ['nama', 'alamat', 'no_telp'];
 }

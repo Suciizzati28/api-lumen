d0dfeb81eee17d7a8809678a6782c0496ed2dde7
PerwiraDhani <perwira.dzakwan11@gmail.com>
2022-12-16 07:15:28 +0700

    update book models

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -3,10 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+// use App\Models\Peminjam;
 
 class Book extends Model
 {
     //
+    public function peminjam()
+    {
+        return $this->belongsTo(Peminjam::class);
+    }
 
     // protected $table = 'books';
     protected $fillable = ['judul', 'penulis', 'genre'];
